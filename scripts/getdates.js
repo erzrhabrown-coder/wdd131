58aa67c27be634f9521a246ee31a15d82ba5b6fe
erzrhabrown <eatobrown@byupathway.edu>
2025-09-05 11:02:26 -0700

    Done

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const currentYear = new Date().getFullYear();
+
+
+document.getElementById('currentyear').textContent = currentYear;
+
+const docMod =(document.lastModified);
+
+document.getElementById('lastModified').textContent =  docMod;
