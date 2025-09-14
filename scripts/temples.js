1c883767bc1be6a182a2d4ab17f41daef0d0aca6
erzrhabrown <eatobrown@byupathway.edu>
2025-09-13 18:20:43 -0700

    done

--- scripts/temples.js
+++ scripts/temples.js
@@ -13,6 +13,7 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
+    
 })
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
