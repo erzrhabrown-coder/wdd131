7c5a032ec4c7d367c57f7d98cd52caac8538426f
erzrhabrown <eatobrown@byupathway.edu>
2026-02-09 17:47:36 -0800

    done

---
+++ Project/siteplan.js
@@ -0,0 +1,8 @@
+const currenty = new Date().getFullYear();
+
+
+document.querySelector("#currentyear").innerHTML = currenty;
+
+const docMod =(document.lastModified);
+
+document.querySelector("#lastModified").textContent =  docMod;
